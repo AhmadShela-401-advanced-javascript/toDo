9e426d95e8e72599381dabc390ea560caf281d22
Ahmad Shela <ahmedsh717@gmail.com>
2020-10-21 00:16:24 +0300

    customHooks

--- src/components/todo/form.js
+++ src/components/todo/form.js
@@ -1,21 +1,29 @@
 import React, { useState } from 'react';
 import {  Form ,Button, Card } from 'react-bootstrap';
+import useForm from '../../hooks/useForm';
+
 
 function TodoForm(props) {
   const [item,setItem] = useState();
+  const [handleSubmit, handleChange, values] = useForm(setItemFun)
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    e.target.reset();
-    console.log(item);
-    props.handleSubmit(item);
-    // const item = {};
+
+  function setItemFun(item) {
+    props.handleSubmit(item)
     setItem({item});
-  };
+  }
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   e.target.reset();
+  //   console.log(item);
+  //   props.handleSubmit(item);
+  //   // const item = {};
+  //   setItem({item});
+  // };
 
-  const  handleInputChange = e => {
-    setItem({...item, [e.target.name]: e.target.value });
-  };
+  // const  handleChange = e => {
+  //   setItem({...item, [e.target.name]: e.target.value });
+  // };
 
   return (
     <>
@@ -27,18 +35,18 @@ function TodoForm(props) {
           <Form.Control
             name="text"
             placeholder="Add To Do List Item"
-            onChange={handleInputChange}
+            onChange={handleChange}
           />
         </Form.Label>
         <Form.Label>
           <span>Difficulty Rating</span>
-          <Form.Control defaultValue="1" type="range" min="1" max="5" name="difficulty" onChange={handleInputChange} />
+          <Form.Control defaultValue="1" type="range" min="1" max="5" name="difficulty" onChange={handleChange} />
         </Form.Label>
         <Form.Label>
           <span>Assigned To</span>
-          <Form.Control type="text" name="assignee" placeholder="Assigned To" onChange={handleInputChange} />
+          <Form.Control type="text" name="assignee" placeholder="Assigned To" onChange={handleChange} />
         </Form.Label>
-        <Button variant="primary">Add Item</Button>{' '}
+        <Button type="submit" variant="primary">Add Item</Button>{' '}
         {/* <button></button> */}
       </Form>
     </Card>
@@ -46,52 +54,34 @@ function TodoForm(props) {
   );
   
 }
-// class TodoForm extends React.Component {
-
-//   constructor(props) {
-//     super(props);
-//     this.state = { item: {} };
-//   }
-
-//   handleInputChange = e => {
-//     this.setState({ item: {...this.state.item, [e.target.name]: e.target.value } });
-//   };
-
-//   handleSubmit = (e) => {
-//     e.preventDefault();
-//     e.target.reset();
-//     console.log(this.state.item);
-//     this.props.handleSubmit(this.state.item);
-//     const item = {};
-//     this.setState({item});
-//   };
-
-//   render() {
-//     return (
-//       <>
-//         <h3>Add Item</h3>
-//         <form onSubmit={this.handleSubmit}>
-//           <label>
-//             <span>To Do Item</span>
-//             <input
-//               name="text"
-//               placeholder="Add To Do List Item"
-//               onChange={this.handleInputChange}
-//             />
-//           </label>
-//           <label>
-//             <span>Difficulty Rating</span>
-//             <input defaultValue="1" type="range" min="1" max="5" name="difficulty" onChange={this.handleInputChange} />
-//           </label>
-//           <label>
-//             <span>Assigned To</span>
-//             <input type="text" name="assignee" placeholder="Assigned To" onChange={this.handleInputChange} />
-//           </label>
-//           <button>Add Item</button>
-//         </form>
-//       </>
-//     );
-//   }
-// }
 
 export default TodoForm;
+
+// import React, {useState} from 'react';
+// import useForm from '../../hooks/useForm';
+// function Food(props) {
+//     // values state
+//     const [formData, setFormData] = useState({});
+//     const [handleSubmit, handleChange, values] = useForm(eat);
+//     function eat(food) { //  JS hoisting 
+//         console.log("fooood @@@@@@@@ ", food);
+//         setFormData(food);
+//     }
+//     return (
+//         <div>
+//             <h2>Food</h2>
+//             <form onSubmit={handleSubmit}>
+//                 <input name="food" type="text" placeholder="Food name" onChange={handleChange} />
+//                 <input name="type" type="text" placeholder="Type" onChange={handleChange}/>
+//                 <input name="price" type="number" placeholder="Price" onChange={handleChange}/>
+//                 <input name="calories" type="number" placeholder="Calories" onChange={handleChange}/>
+//                 <button>Eat</button>
+//             </form>
+//             <strong>Form Food Values: </strong>
+//             {
+//                 Object.keys(formData).map((key, idx)=> <div key={idx}> {key} - {formData[key]}</div>)
+//             }
+//         </div>
+//     )
+// }
+// export default Food;
