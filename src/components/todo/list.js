305e125348ffb8ec40d45d842484f7ed6df743c8
Ahmad Shela <ahmedsh717@gmail.com>
2020-10-21 03:21:23 +0300

    context

--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import { ListGroup } from 'react-bootstrap';
+import { Form, Button, Card, Dropdown, ListGroup } from 'react-bootstrap';
+import { If, Then, Else } from '../if';
+import React, { useContext } from 'react';
+import { SiteContext } from '../../context/site';
 
 function TodoList(props) {
 
-return (
-  <ListGroup className='ListGroup' variant="flush">
-    {props.list.map(item => (
-      <ListGroup.Item className={`listGroupItem complete-${item.complete.toString()}`}
-        key={item._id} >
+    const siteContext = useContext(SiteContext);
+
+    function testConsoleLog() {
+      console.log('=====================??????');
+    }
+
+    return (
+  <If condition={siteContext.displayCompletedItems == true}>
+    <Then>
+    <ListGroup className='ListGroup' variant="flush">
+    {props.list.map((item,idx)=>(
+        <ListGroup.Item className={`listGroupItem complete-${item.complete.toString()}`}
+        key={idx} >
         <span onClick={() => props.handleComplete(item._id)}>
         {item.text} 
         {item.complete} 
@@ -17,6 +27,24 @@ return (
       </ListGroup.Item>
     ))}
   </ListGroup>
+    </Then>
+    <Else>
+  <ListGroup className='ListGroup' variant="flush">
+    {props.list.filter((item) => (
+      item.complete==false)).map((item,idx)=>(
+        <ListGroup.Item className={`listGroupItem complete-${item.complete.toString()}`}
+        key={idx} >
+        <span onClick={() => props.handleComplete(item._id)}>
+        {item.text} 
+        {item.complete} 
+        {item.difficulty} 
+        {item.assignee} 
+        </span>
+      </ListGroup.Item>
+    ))}
+  </ListGroup>
+    </Else>
+  </If>
 );
 }
 
